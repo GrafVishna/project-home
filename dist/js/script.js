97740ce02e8ce68c781a363904660bb7c9da07c6
GrafVishna <grafvishna4@gmail.com>
2022-06-12 15:03:14 +0300

    new-test

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,16 @@
-//----clone
+
 var header = $('.js-header-fixed');
 cloneHeader = header.clone();
+
 
 cloneHeader.addClass('header--fixed');
 header.before(cloneHeader);
 
-// scroll anim header
+
 $(window).scroll(function () {
+
 	console.log($(window).scrollTop());
+
 	if ($(window).scrollTop() > 350) {
 		cloneHeader.addClass('header--is-show');
 	} else {
@@ -16,11 +19,22 @@ $(window).scroll(function () {
 
 });
 
-// ---burger
-
 $(document).ready(function () {
 	$('.burger').click(function (event) {
 		$('.burger, .header__menu-links').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+});
+
+$(".scrollto a").on("click", function () {
+	let href = $(this).attr("href");
+
+	$("html, body").animate({
+		scrollTop: $(href).offset().top
+	}, {
+		duration: 370,   // по умолчанию «400»
+		easing: "linear" // по умолчанию «swing»
+	});
+
+	return false;
 });
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -25,3 +25,16 @@ $(document).ready(function () {
 		$('body').toggleClass('lock');
 	});
 });
+
+$(".scrollto a").on("click", function () {
+	let href = $(this).attr("href");
+
+	$("html, body").animate({
+		scrollTop: $(href).offset().top
+	}, {
+		duration: 370,   // по умолчанию «400»
+		easing: "linear" // по умолчанию «swing»
+	});
+
+	return false;
+});
